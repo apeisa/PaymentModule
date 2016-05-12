35f78ce83fe5849c9067026d27e745dddc6b779e
Antti Peisa <antti.peisa@gmail.com>
2016-05-12 09:56:29 +0300

    Support for taxes on payment modules

--- PaymentProduct.php
+++ PaymentProduct.php
@@ -1,10 +1,11 @@
 <?php
 
 class PaymentProduct extends WireData {
-  public function __construct($title, $amount, $quantity = 1) {
+  public function __construct($title, $amount, $quantity = 1, $tax = null) {
     $this->title = $title;
     $this->amount = $amount;
     $this->quantity = $quantity;
+    $this->tax = $tax;
     $this->total = $amount * $quantity;
   }
 }
