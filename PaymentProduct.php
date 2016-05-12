17310e34e7e5862f12ab252fe59f7798707d4c7b
Antti Peisa <antti.peisa@gmail.com>
2016-05-12 14:51:23 +0300

    add id

--- PaymentProduct.php
+++ PaymentProduct.php
@@ -1,11 +1,12 @@
 <?php
 
 class PaymentProduct extends WireData {
-  public function __construct($title, $amount, $quantity = 1, $tax_percentage = null) {
+  public function __construct($title, $amount, $quantity = 1, $tax_percentage = null, $id = null) {
     $this->title = $title;
     $this->amount = $amount;
     $this->quantity = $quantity;
     $this->tax_percentage = $tax_percentage;
+    $this->id = $id;
     $this->total = $amount * $quantity;
   }
 }
