494bb11089e23b8a912bb161e6a8df7ca6717cbc
Antti Peisa <antti.peisa@gmail.com>
2016-05-12 10:04:49 +0300

    Change variable name

--- PaymentProduct.php
+++ PaymentProduct.php
@@ -1,11 +1,11 @@
 <?php
 
 class PaymentProduct extends WireData {
-  public function __construct($title, $amount, $quantity = 1, $tax = null) {
+  public function __construct($title, $amount, $quantity = 1, $tax_percentage = null) {
     $this->title = $title;
     $this->amount = $amount;
     $this->quantity = $quantity;
-    $this->tax = $tax;
+    $this->tax_percentage = $tax_percentage;
     $this->total = $amount * $quantity;
   }
 }
